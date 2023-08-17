a4bd5c195d50a5e68c20844bbb4f68434aa9d31f
giftjira <thunyasiri.jir@gmail.com>
2023-08-17 21:13:00 +0700

    Add variable

--- Exercise4_Thunyasiri_J.py
+++ Exercise4_Thunyasiri_J.py
@@ -1,9 +1,10 @@
-English = 85.5
-Business = 90
-Computer = 89.5
-Programming = 91.5
-print('---Your Score---')
-print('Foundation English : ',English)
-print('General Business : ',Business)
-print('Introduction to Computer Systems : ',Computer)
-print('Computer Programming : ',Programming)
+Foud_E = 78.5
+Gen_B = 89.0
+Intro_C = 80.0
+Com_P = 99.0
+
+print("--- Your Score ---")
+print("Foundation English : Foud_E")
+print("General Business : Gen_B")
+print("Introduction to Computer Systems : Intro_C")
+print("Computer Programming : Com_P")
